273124d5776bc5d754e681153f765b90c7cd4d64
aumaam255789 <65010837@kmitl.ac.th>
2024-06-18 13:26:13 +0700

    Update main.py

---
+++ rush/ex00/main.py
@@ -0,0 +1,27 @@
+
+
+"""#เงื่อนไขหารสร้างฟังก์ชั่น#
+
+-ตรวจว่ารุกฆาตไหม
+
+-ตารางจตุรัส น้อยกว่า 8*8
+
+-มี เบี้ย ราชา ราชินี เรือ บิช็อป (ไม่มีม้า)
+
+-A piece can only capture the first possible piece that stands on its path.
+แปลว่าอะไรนิ? ดำน้ำ-หมากแต่ละตัวต้องอยู่ในตำแหน่งเริ่ม? หรือ หมากแต่ละตัวต้องอยู่จุดที่เดินไปได้จากตำแหน่งเริ่มเท่านั้น
+
+-all other pieces are against it. All the characters that are not used to refer to pieces are considered as empty squares.
+แปลไม่ได้อีกละ
+
+-ถ้ารุกฆาตได้ให้ขึ้นบรรทัดใหม่ว่า "Success" ถ้าไม่ได้ให้ขึ้นว่า "Fail"
+
+-ฟังก์ชั่นต้องไม่หยุดเองหรือวนซ้ำไม่หยุด
+
+-Whenever an undefiend behavior occurs you should return an error message or
+prints nothing and gives back control.
+
+#ทำความเข้าใจ#
+-2ฝั่ง 1.คิง  2.ตัวที่เหลือ
+
+-เช็คว่า 1 ช่องรอบคิงเป็น x รึเปล่า
